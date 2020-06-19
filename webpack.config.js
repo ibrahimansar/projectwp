43be101c3a4e5875ed87c3a7194ec1cf93470a99
Mohamed Ibrahim Ansari <54184745+ibrahimansar@users.noreply.github.com>
2020-06-19 23:40:46 +0530

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const autoprefixer = require('autoprefixer');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: ''
+    },
+    devtool: 'cheap-module-eval-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                options: {
+                    importLoaders: 1,
+                    modules: {
+                        localIdentName: '[name]_[local]_[hash:base64:5]'
+                    }
+                }
+            },
+            { 
+                loader: 'postcss-loader', options: {
+                ident: 'postcss',
+                plugind: () => [autoprefixer]
+            }
+        },
+        { test: /\.(png|jpe?g|gif)$/,
+        loader: 'url-loader?limit=80007name=images/[name].[text]'}            
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: __dirname + '/src/index.html',
+            filename: 'index.html',
+            inject: 'body'
+        })
+    ]
+};
